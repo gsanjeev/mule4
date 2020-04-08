9e15776dcb016904a1de2ef4db407d715aedbc7c
Sanjeev Tripathi <gsanjeevtripathi@gmail.com>
2020-04-08 15:01:49 +0530

    custom-policy initial commit

---
+++ custom-policy/good-weather/arch-type.bat
@@ -0,0 +1,1 @@
+mvn -Parchetype-repository archetype:generate -DarchetypeGroupId=org.mule.tools -DarchetypeArtifactId=api-gateway-custom-policy-archetype -DarchetypeVersion=1.2.0 -DgroupId=6ecab386-da25-46f2-abed-b71a7b6964ac -DartifactId=greet-response-policy -Dversion=1.0.0 -Dpackage=mule-policy
