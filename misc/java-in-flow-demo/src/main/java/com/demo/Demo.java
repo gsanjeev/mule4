9ed8f5883c98472867c4279df73bece7172f7481
1028586 <sanjeev.tripathi@jda.com>
2020-10-18 15:59:25 +0530

    java intial commit

---
+++ misc/java-in-flow-demo/src/main/java/com/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Demo {
+	
+	public static String greet(String name) {
+		return "Greeting " + name + "!";
+	}
+	
+	public String getFullName(String fname, String lname) {
+		return fname + " " + lname;
+	}
+
+}
